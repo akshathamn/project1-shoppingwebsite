e5fd703baaa4784b68b90db875ab3b50677f9ae5
akshathamn <akshathamn05@gmail.com>
2019-07-25 17:15:17 +0530

    commited

--- src/Component/Login.js
+++ src/Component/Login.js
@@ -2,18 +2,22 @@ import React, { Component } from "react";
 // import nature from '../images/nature.jpg';
 import { Button, FormGroup, FormControl } from "react-bootstrap";
 import "./Signup.css";
-import {login} from './UserFunction';
-import {connect} from 'react-redux'; 
+import { login } from './UserFunction';
+import { connect } from 'react-redux';
 import { log } from '../Action/Loginaction';
 
- class Login extends Component {
+class Login extends Component {
   constructor(props) {
     super(props);
-
     this.state = {
       email: "",
-      password: ""
+      password: "",
+      emailerr: "",
+      pswd: ""
+      // submitted: false
     };
+    // this.handleChange = this.handleChange.bind(this);
+    // this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   validateForm() {
@@ -28,32 +32,45 @@ import { log } from '../Action/Loginaction';
 
   handleSubmit = event => {
     event.preventDefault();
-  // }
-  const {  email, password } = this.state;
+    const { email, password } = this.state;
     this.setState({ submitted: true });
     // if (firstName && username && email && password && confirmPassword && mobileNumber) {
     // this.props.history.push('/login');
-    
+
     // }
     const user = {
-    
-    email: this.state.email,
-    password: this.state.password,
-    
-     }
+      email: this.state.email,
+      password: this.state.password,
+
+    }
     login(user).then(res => {
-    if (email&&password ) {
-    this.props.history.push('/Home')
-    }
+      if (email && password) {
+        this.props.history.push('/Home')
+      }
     })
+    let temp1 = 0;
+    let emailerr = this.state.email.length, pswd = this.state.password.length;
+    // let reg_name = /^[A-Za-z]{2,10}$/;
+    let reg_pswd = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,15})/;
+    let reg_email = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    // let reg_phn = /^[0-9]{10}$/;
+
+    if (emailerr === 0) this.setState({ emailerr: '*Email is required' });
+    else if (!reg_email.test(this.state.email)) this.setState({ emailerr: '*Invalid Email' });
+    else temp1++;
+    if (pswd === 0) this.setState({ pswd: '*Password is required' });
+    else if (!reg_pswd.test(this.state.password)) this.setState({ pswd: '*Invalid Password' });
+    else temp1++;
+    if (temp1 > 3) {
+      this.props.history.push('/login')
     }
+  }
 
   render() {
     return (
       <div className="Login bg-img">
-          <form onSubmit={this.handleSubmit}>
+        <form onSubmit={this.handleSubmit}>
           {/* <img className="col-md-12 col-lg-12" src={nature} alt="logo"/> */}
-        
           <FormGroup controlId="email" bsSize="large">
             <p>Email</p>
             <FormControl
@@ -62,6 +79,10 @@ import { log } from '../Action/Loginaction';
               value={this.state.email}
               onChange={this.handleChange}
             />
+            <p className='msg'>{this.state.emailerr}</p><br></br>
+            {/* {submitted && !email &&
+                    <div className="help-block">email is required</div>
+                  } */}
           </FormGroup>
           <FormGroup controlId="password" bsSize="large">
             <p>Password</p>
@@ -70,6 +91,7 @@ import { log } from '../Action/Loginaction';
               onChange={this.handleChange}
               type="password"
             />
+            <p className='msg'>{this.state.pswd}</p><br></br>
           </FormGroup>
           <Button
             block
@@ -79,9 +101,7 @@ import { log } from '../Action/Loginaction';
           >
             Login
           </Button>
-         
         </form>
-       
       </div>
     );
   }
@@ -89,7 +109,18 @@ import { log } from '../Action/Loginaction';
 const mapStateToProps = (state) => {
   const { email } = state.Loginreducer;
   const { password } = state.Loginreducer;
-  return {  email, password };
+  return { email, password };
 
 };
 export default connect(mapStateToProps, { log })(Login);
+
+
+
+
+
+
+
+
+
+
+
--- src/Component/Signup.js
+++ src/Component/Signup.js
@@ -2,8 +2,8 @@ import React, { Component } from "react";
 import { Button, FormGroup, FormControl } from "react-bootstrap";
 import "./Signup.css";
 import { sign } from '../Action/Signupaction';
-import {signup} from './UserFunction';
-import {connect} from 'react-redux'; 
+import { signup } from './UserFunction';
+import { connect } from 'react-redux';
 class Signup extends Component {
   constructor(props) {
     super(props);
@@ -11,7 +11,10 @@ class Signup extends Component {
     this.state = {
       email: "",
       password: "",
-      confirmpassword: ""
+      confirmpassword: "",
+      emailerr: "",
+      pswd: "",
+      cpswd: ""
     };
   }
 
@@ -27,29 +30,49 @@ class Signup extends Component {
 
   handleSubmit = event => {
     event.preventDefault();
-  // }
- 
-    const {  email, password, confirmpassword } = this.state;
+    // }
+
+    const { email, password, confirmpassword } = this.state;
     this.setState({ submitted: true });
     // if (firstName && username && email && password && confirmPassword && mobileNumber) {
     // this.props.history.push('/login');
-    
+
     // }
     const user = {
-    
-    email: this.state.email,
-    password: this.state.password,
-    confirmpassword: this.state.confirmpassword,
-     }
-     signup(user).then(res => {
-    if (email&&password && confirmpassword) {
-    this.props.history.push('/login')
+      email: this.state.email,
+      password: this.state.password,
+      confirmpassword: this.state.confirmpassword,
     }
+    signup(user).then(res => {
+      if (email && password && confirmpassword) {
+        this.props.history.push('/login')
+      }
     })
+
+
+    let temp1 = 0;
+    let emailerr = this.state.email.length, pswd = this.state.password.length, cpswd = this.state.confirmpassword.length;
+    // let reg_name = /^[A-Za-z]{2,10}$/;
+    let reg_pswd = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,15})/;
+    let reg_cpswd = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,15})/;
+    let reg_email = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    if (emailerr === 0) this.setState({ emailerr: '*Email is required' });
+    else if (!reg_email.test(this.state.email)) this.setState({ emailerr: '*Invalid Email' });
+    else temp1++;
+    if (pswd === 0) this.setState({ pswd: '*Password is required' });
+    else if (!reg_pswd.test(this.state.password)) this.setState({ pswd: '*Invalid Password' });
+    else temp1++;
+    if (cpswd === 0) this.setState({ cpswd: '*confirmpassword is required' });
+    else if (!reg_cpswd.test(this.state.confirmpassword)) this.setState({ cpswd: '*Invalid Password' });
+    else temp1++;
+    if (temp1 > 3) {
+      this.props.history.push('/login')
     }
+  }
+
   render() {
     return (
-
       <div className="Login bg-signup">
         <form onSubmit={this.handleSubmit}>
           <FormGroup controlId="email" bsSize="large">
@@ -60,6 +83,7 @@ class Signup extends Component {
               value={this.state.email}
               onChange={this.handleChange}
             />
+            <p className='msg'>{this.state.emailerr}</p><br></br>
           </FormGroup>
           <FormGroup controlId="password" bsSize="large">
             <p>Password</p>
@@ -68,6 +92,7 @@ class Signup extends Component {
               onChange={this.handleChange}
               type="password"
             />
+            <p className='msg'>{this.state.pswd}</p><br></br>
           </FormGroup>
           <FormGroup controlId="confirmpassword" bsSize="large">
             <p>ConfirmPassword</p>
@@ -76,12 +101,13 @@ class Signup extends Component {
               onChange={this.handleChange}
               type="password"
             />
+            <p className='msg'>{this.state.cpswd}</p><br></br>
           </FormGroup>
           <Button
             block
             bsSize="large"
-            disabled={!this.validateForm()}
-            type="submit" 
+            // disabled={!this.validateForm()}
+            type="submit"
           >
             Signup
           </Button>
@@ -92,11 +118,10 @@ class Signup extends Component {
 }
 const mapStateToProps = (state) => {
   const { email } = state.Signupreducer;
-
   const { password } = state.Signupreducer;
   const { confirmpassword } = state.Signupreducer;
   // const{message}=state.Registerreducer;
-  return {  email, password,confirmpassword };
+  return { email, password, confirmpassword };
 
 };
 export default connect(mapStateToProps, { sign })(Signup);
