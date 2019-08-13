54cfc7a7107f2d9113bf8767ccb8880a6878ef5c
akshathamn <akshathamn05@gmail.com>
2019-08-13 17:16:25 +0530

    commited

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -5,50 +5,50 @@ import './Home.css';
 import Navbar from './Navbar';
 
 class Home extends Component {
-  // render() {
-  //   return (
-      // <div>
-       
-      //   <img className="col-md-12 col-lg-12" src={shop} alt="logo" />
-      // </div>
+  render() {
+    return (
+      <div>
+       <Navbar />
+        <img className="col-md-12 col-lg-12" src={shop} alt="logo" />
+      </div>
 
 
-      render() {
+      // render() {
 
-        return (
+      //   return (
     
 
-            <div className='slidebody'>
-                <Navbar />
-                {/* <img className="col-md-12 col-lg-12" src={shop} alt="logo" /> */}
-                <MDBContainer>
-                    <MDBCarousel
-                        activeItem={1}
-                        length={10}
-                        showControls={true}
-                        showIndicators={false}
-                        className="z-depth-1">
-                        <MDBCarouselInner>
-                            <MDBCarouselItem itemId="1">
-                                <MDBView>
-                                    <img className="d-block w-60 slide" src={require('../images/offer1.jpg')} alt="First slide" />
-                                </MDBView>
-                            </MDBCarouselItem>
-                            <MDBCarouselItem itemId="2">
-                                <MDBView>
-                                    <img className="d-block w-60 slide" src={require('../images/offer2.jpg')} alt="Second slide" />
-                                </MDBView>
-                            </MDBCarouselItem>
-                            <MDBCarouselItem itemId="3">
-                                <MDBView>
-                                    <img className="d-block w-60 slide" src={require('../images/offer3.jpg')} alt="Third slide"/>
-                                </MDBView>
-                            </MDBCarouselItem>
+            // <div className='slidebody'>
+            //     <Navbar />
+            //     {/* <img className="col-md-12 col-lg-12" src={shop} alt="logo" /> */}
+            //     <MDBContainer>
+            //         <MDBCarousel
+            //             activeItem={1}
+            //             length={10}
+            //             showControls={true}
+            //             showIndicators={false}
+            //             className="z-depth-1">
+            //             <MDBCarouselInner>
+            //                 <MDBCarouselItem itemId="1">
+            //                     <MDBView>
+            //                         <img className="d-block w-60 slide" src={require('../images/offer1.jpg')} alt="First slide" />
+            //                     </MDBView>
+            //                 </MDBCarouselItem>
+            //                 <MDBCarouselItem itemId="2">
+            //                     <MDBView>
+            //                         <img className="d-block w-60 slide" src={require('../images/offer2.jpg')} alt="Second slide" />
+            //                     </MDBView>
+            //                 </MDBCarouselItem>
+            //                 <MDBCarouselItem itemId="3">
+            //                     <MDBView>
+            //                         <img className="d-block w-60 slide" src={require('../images/offer3.jpg')} alt="Third slide"/>
+            //                     </MDBView>
+            //                 </MDBCarouselItem>
                             
-                        </MDBCarouselInner>
-                    </MDBCarousel>
-                </MDBContainer>
-            </div>
+            //             </MDBCarouselInner>
+            //         </MDBCarousel>
+            //     </MDBContainer>
+            // </div>
         );
     }
 }
